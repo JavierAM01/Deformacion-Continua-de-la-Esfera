0d574eec45cb889cbd876e8fd0d8a56e70657025
Javier <javierabollado@gmail.com>
2023-04-08 20:38:09 +0200

    Nuevos difeomorfismos y creación de los gif finales

--- difeomorfismos.py
+++ difeomorfismos.py
@@ -2,10 +2,31 @@ import numpy as np
 
 
 
+
+
+# [0,1] -> [0,1) deformaciones del intervalo [0,1]
+f1 = lambda t : 2./np.pi*np.arctan(np.tan(np.pi*t/2.0)/20.)
+f2 = lambda t : 2./np.pi*np.arctan(np.tan(np.pi*t/2.0)*20.)
+
 def dif1(x, y, z, t):
+    c = 1.0 / (1.0 - z*t)
+    xt = x * c
+    yt = y * c
+    zt = z * (1-t) + (-1) * t
+    return xt, yt, zt
+
+def dif2(x, y, z, t):
+    c = 1.0 / (1.0 - z*t)    
+    xt = x * c
+    yt = y * c
+    zt = z / (1-t*z) * (1-t) + (-1) * t
+
+    return xt, yt, zt
+
+def dif3(x, y, z, t):
 
     c = 1.0 / (1.0 - z*t)
-    k = 2./np.pi*np.arctan(np.tan(np.pi*t/2.0)/20.) # [0,1] -> [0,1) de forma que se mantiene en 0 la mayoría del intervalo
+    k = f1(t) 
     
     xt = x * c
     yt = y * c
@@ -13,42 +34,31 @@ def dif1(x, y, z, t):
 
     return xt, yt, zt
 
-def dif2(x, y, z, t):
+def dif4(x, y, z, t):
 
-    t1 = lambda x : np.pi/2. * x                    # X : [-1,1]       -> [pi/2, pi/2]
-    t2 = lambda x : np.tan(x)                       # X : [pi/2, pi/2] -> (-infty, +infty)
-    aux = lambda x, t : x * (1-t) + t2(t1(x)) * t   # X : [-1,1]       ~> [1, +infinito)*sign(x)     // el factor t lo hace progresivo
-
-    xt = aux(x, t)
-    yt = aux(y, t)
-    k = 2./np.pi*np.arctan(np.tan(np.pi*t/2.0)/20.) # [0,1] -> [0,1) de forma que se mantiene en 0 la mayoría del intervalo
-    zt = z / (1-t*z) * (1-k) + (-1) * k
-    return xt, yt, zt
-
-def dif3(x, y, z, t):
-
+    w = 1 # 10, 0.1, ...
     t1 = lambda x : np.pi/2. * (x + 1) /2.          # X : [-1,1]    -> [0, pi/2]
-    t2 = lambda x : 10*np.tan(x)                    # X : [0, pi/2] -> [0, +infty)
+    t2 = lambda x : np.tan(x) * w                   # X : [0, pi/2] -> [0, +infty)
     aux = lambda x, t : 1 + t2(t1(x)) * t           # X : [-1,1]    ~> [1, +infinito)     // el factor t lo hace progresivo
 
     c = aux(z, t)
     xt = x * c
     yt = y * c
-    k = 2./np.pi*np.arctan(np.tan(np.pi*t/2.0)/20.) # [0,1] -> [0,1) de forma que se mantiene en 0 la mayoría del intervalo
-    zt = z / (1-t*z) * (1-k) + (-1) * k
-    return xt, yt, zt
-
-def dif4(x, y, z, t):
-    xt = np.tan(np.arctan(x/z)) * t + x * (1-t)
-    yt = np.tan(np.arctan(y/z)) * t + y * (1-t)
-    zt = z * (1-t) + (-1) * t
+    zt = z / (1-t*z) * (1-t) + (-1) * t
     return xt, yt, zt
 
 def dif5(x, y, z, t):
-    c = 1.0 / (1.0 - z*t)
+
+    w = 1 # 10, 0.1, ...
+    t1 = lambda x : np.pi/2. * (x + 1) /2.          # X : [-1,1]    -> [0, pi/2]
+    t2 = lambda x : np.tan(x) * w                   # X : [0, pi/2] -> [0, +infty)
+    aux = lambda x, t : 1 + t2(t1(x)) * t           # X : [-1,1]    ~> [1, +infinito)     // el factor t lo hace progresivo
+
+    k = f1(t)  
+    c = aux(z, k)
     xt = x * c
     yt = y * c
-    zt = z * (1-t) + (-1) * t
+    zt = z / (1-t*z) * (1-t) + (-1) * t
     return xt, yt, zt
 
 def dif6(x, y, z, t):
--- main.py
+++ main.py
@@ -47,23 +47,39 @@ def update(t, x0, y0, z0, x02, y02, z02, ax, dif):
     filter = np.apply_along_axis(all, 1, (xt**2 + yt**2 + zt**2 < 4**2))
     
     # plot
-    ax.plot_surface(xt[filter], yt[filter], zt[filter], cmap=plt.cm.viridis, alpha=0.8)
+    ax.plot_surface(xt[filter], yt[filter], zt[filter], cmap=plt.cm.viridis, alpha=1)
     #ax.scatter(x2t, y2t, z2t, s=2, c=plt.cm.ScalarMappable(cmap='magma').to_rgba(z2t), zorder=1)
 
 
-def main():
+def animate_sphere(difeomorfismo, save=False, savepath="ani.gif"):
+
     n_frames = 50
     x, y, z = createSphere()
     x2, y2, z2 = createEspiral()
 
-    midif = dif6
-
     fig = plt.figure()
     ax = fig.add_subplot(111, projection="3d")
     ax.view_init(elev=15, azim=-45)
-    _ = animation.FuncAnimation(fig, update, frames=np.linspace(0,1,n_frames), fargs=[x, y, z, x2, y2, z2, ax, midif], interval=100)
+    ani = animation.FuncAnimation(fig, update, frames=np.linspace(0,1,n_frames), fargs=[x, y, z, x2, y2, z2, ax, difeomorfismo], interval=100)
+    if save:
+        ani.save(f"images/{savepath}")
+    plt.show()
+
+def plot_interval(function, a=0, b=1):
+    c, d = int(np.round(function(a))), int(np.round(function(b)))
+    x = np.linspace(a, b, 100)
+    y = function(x)
+    fig = plt.figure(figsize=(5,5))
+    ax = fig.add_subplot(111)
+    ax.set_title(f"Conversión de intervalos:\n[{a},{b}] -> [{c},{d}]")
+    ax.plot(x, y, "k-")
+    ax.set_xticks([a,b])
+    ax.set_yticks([c, d])
     plt.show()
 
 
 if __name__ == "__main__":
-    main()
+    for i, dif in enumerate([dif1, dif2, dif3, dif4, dif5, dif6]):
+        animate_sphere(dif, save=True, savepath=f"difeomorfismo_{i+1}.gif")
+    # plot_interval(f1)
+    # plot_interval(f2)
